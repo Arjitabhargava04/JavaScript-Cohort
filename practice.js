61f9cc56d49ffdc50b1399958ef02f5e6af4f29d
Arjitabhargava04 <aashiibharadwaj728@gmail.com>
2025-12-03 13:38:56 +0530

    first commit

---
+++ practice.js
@@ -0,0 +1,24 @@
+/*
+let x= 5;
+let y = x++ + ++x;
+console.log(x, y); // 7  12
+
+*/
+
+
+/*
+let x= 4;
+let y = --x + x-- + ++x;  // 3  9
+console.log(x, y);
+
+*/
+
+ /*
+let x=2;
+let y = x++ - --x  + ++x  - x--; // 2,  0
+
+*/
+
+let i= 1;
+console.log(++i);  // 2
+console.log(i++);  // 2 (but i is now 3)
